329944681c935139776838872cb33b7f30e26195
mah-creator <85145793+mah-creator@users.noreply.github.com>
2025-01-14 15:07:39 +0200

    Using the range operator

--- script.ps1
+++ script.ps1
@@ -1,5 +1,5 @@
 $CHROME_EXE = "C:\Program Files\Google\Chrome\Application\chrome.exe"
-$profileIDs = @(11, 13, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29)
+$profileIDs = (11, 13) + (16..29)
 
 foreach ($i in $profileIDs) {
     & $CHROME_EXE "google.com" --profile-directory="Profile $i"
